20ab9bced606d687a193349a4e5752dc14aac177
Vince <vincentfiorilli@gmail.com>
2018-07-04 08:51:43 -0400

    ran into wall with inquirer for loop

--- bandMembers/main.js
+++ bandMembers/main.js
@@ -15,8 +15,33 @@ inquirer.prompt([
       game()
     }
 })
+function loop() {
+if (page <= last_page) {
+    request("/data?page=" + page, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            store_data(body)
+        }
+        page++;
+        loop();
+    });
+}
+};
+//next we need to loop through answers.numBandMembers and create them with inquirer
+//let bandMemberCount = 4
+function createMembers(){
 
-
+  //if(bandMemberCount > 0){
+    inquirer.prompt([
+      {
+        type: 'input',
+        name: 'memberName',
+        message: 'ENTER BAND MEMBER NAME:'
+      }
+    ]).then(answers => {
+      console.log(answers)
+    })
+//}
+}
 
 function game(){
   inquirer.prompt([
@@ -25,21 +50,25 @@ function game(){
       name: 'bandName',
       message: 'What is the band name?'
     },
-    {
+    /*{
       type: 'input',
       name: 'numBandMembers',
       message: 'How many members are in the band?'
-    },
+    },*/
     {
       type: 'input',
       name: 'bandGenre',
       message: 'What is your genre of music?'
     }
   ]).then(answers => {
-    console.log(JSON.stringify(answers))
+    //console.log(JSON.stringify(answers))
     //create a new Band object with the answers
-    var newBand = new Band(answers.bandName, answers.numBandMembers, answers.bandGenre)
-    console.log('new band added ' + JSON.stringify(newBand))
+    var newBand = new Band(answers.bandName, answers.bandGenre)
+    console.log('Your band ' + answers.bandName.toUpperCase() + ' has been created')
+    //let bandMemberCount = answers.numBandMembers
+
+    //this should be a recursive function
+    createMembers()
+
   })
-  console.log('test')
 }
