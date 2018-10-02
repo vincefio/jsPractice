6d3872a6dda8b0208296d46380b08beebb07edd8
Vince <vincentfiorilli@gmail.com>
2018-10-02 19:22:39 -0400

    c

--- hangman/game.js
+++ hangman/game.js
@@ -1,6 +1,8 @@
 $(document).ready(() => {
 
     let game = {
+        word: '',
+
         //declare words and their hints
         guessWords: [
             {
@@ -14,15 +16,29 @@ $(document).ready(() => {
             {
                 title: 'satisfaction',
                 hint: 'The Rolling Stones First American Hit Single released in 1965'
-            }
+            }/*,
+            {
+                title: 'cruisin'
+                hint
+            }*/
         ],
 
 
-        randomWord: () => {
+        randomWord: function () {
             //create random number between 0 and 3
-            //Math.floor(Math.random() * 3)
+            this.word = this.guessWords[Math.floor(Math.random() * 3)].title
+            console.log('CURRENT WORD IS ' + this.word.toUpperCase())
+            console.log('-------------------------------')
+            return this.word
+        },
+
+        //create function to update guesses div
+        updateGuesses: function () {
+            console.log(this.word)
         }
     }
 
-    console.log(game.guessWords[1])
+    console.log(game.randomWord())
+    //console.log(game.word)
+    game.updateGuesses()
 })
